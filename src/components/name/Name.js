de61c92f9652b4e36a2ef54fed875773c57a6e77
Christian <ChrisWcs@users.noreply.github.com>
2017-09-26 22:38:01 -0600

    Add components

---
+++ src/components/name/Name.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import "./Name.css";
+
+const Name = ({name}) => (
+    <h1 className="">{name}</h1>
+);
+
+export default Name;
---
+++ src/components/picture/Picture.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ImgSrc from './rocket.jpg';
+import "./Picture.css";
+
+const Picture = () => (
+    <img src={ImgSrc} className="Picture"/>
+);
+
+export default Picture;
